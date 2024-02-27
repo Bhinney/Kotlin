71c5e7088457f6d1707e010cbd8cbb35533cd5f2
Bhinney <snakellie97@gmail.com>
2024-02-27 15:52:59 +0900

    #2 : 알고리즘 풀기

---
+++ Study_Kotlin/src/main/java/알고리즘/gold/BJ12015_증가하는_긴_부분수열_자바.java
@@ -0,0 +1,35 @@
+package 알고리즘.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BJ12015_증가하는_긴_부분수열_자바 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		List<Integer> ans = new ArrayList<>();
+		ans.add(arr[0]);
+		for (int i = 1; i < n; i++) {
+			int value = arr[i];
+
+			if (ans.get(ans.size() - 1) < value) ans.add(value);
+			else {
+				int min = 0, max = ans.size() - 1;
+				while (min < max) {
+					int mid = (max + min) / 2;
+					if (ans.get(mid) >= value) max = mid;
+					else min = mid + 1;
+				}
+				ans.set(max, value);
+			}
+		}
+
+		System.out.println(ans.size());
+	}
+}
---
+++ Study_Kotlin/src/main/kotlin/알고리즘/gold/BJ12015_증가하는_긴_부분수열.kt
@@ -0,0 +1,11 @@
+package 알고리즘.gold
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toLong()
+    val arr = readLine().split(" ").map { it.toLong() }
+
+
+}
+
+class BJ12015_증가하는_긴_부분수열 {
+}
